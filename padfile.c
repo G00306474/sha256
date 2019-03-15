3f91e7634508016a72618e98dd1de464ed1648ab
Kevin Moran <g00306474@gmit.ie>
2019-03-15 13:12:03 +0000

    Adds padding to a message where final block less than 56 bytes

--- padfile.c
+++ padfile.c
@@ -10,19 +10,34 @@ union msgblock {
 int main(int argc, char *argv[]){
 
   union msgblock M;
+  uint64_t nobits = 0; 
   uint64_t nobytes;
   
   FILE* f;
-  
-  
   f = fopen(argv[1], "r");
   
-  while (!feof(f)){
-
-   nobytes =  fread(M.e, 1, 64,f);
-  printf("%llu\n",nobytes);
+  while (!feof(f)){  
+    nobytes =  fread(M.e, 1, 64,f);
+    printf("Read %2llu bytes \n", nobytes);
+    nobits = nobits +  (nobytes * 8);
+    
+    if (nobytes < 56){
+      printf("Block of less than 55 bytes!\n");   
+      M.e[nobytes] = 0x80; 
+      while (nobytes < 56) {
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00; 
+      }
+      M.s[7] = nobits;
+    }
   }
 
   fclose(f);
-  return 0; 
+  
+  for (int i = 0; i < 64; i++){
+    printf("%x ",M.e[i]);
+   }
+   printf("\n");
+   
+ return 0; 
 }
